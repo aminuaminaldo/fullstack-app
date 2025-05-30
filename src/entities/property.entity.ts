7806ac928f78ab46fb0e6b0149ba98e92262303b
Aminu Aminaldo <90836602+aminuaminaldo@users.noreply.github.com>
2025-05-30 17:24:42 +0300

    created the first entity

---
+++ src/entities/property.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Property {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column({ default: 0 })
+  price: number;
+}
